cccc974c70d514444b4f576addc48c276819a48b
Gabriel Torres <gabrieltlpires@gmail.com>
2023-05-09 21:23:07 -0300

    Adiciona solução do exercício

---
+++ index.js
@@ -0,0 +1,6 @@
+var foto = window.document.getElementById('foto')
+
+function trocarImagem(){
+    foto.innerHTML = `<img src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI-2rjKA8EaTNuZCHPRIlp9fKnOgww8rhmcA&usqp=CAU" alt="imagem de um cachorro fantasiado de leão">`
+    foto.innerHTML += `<p>grrrrrrrrrrrrrrrrrrr!</p>`
+}
